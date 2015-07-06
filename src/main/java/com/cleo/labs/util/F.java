bec4764a0aedb8623f7966b2139aa5afaeb9e6d8
John Thielens <jthielens@jthielens1.lan.cleo.com>
2015-07-06 11:18:27 -0400

    add Javadocs, User-Agent for F.download to get what we need from nexus

--- src/main/java/com/cleo/labs/util/F.java
+++ src/main/java/com/cleo/labs/util/F.java
@@ -14,8 +14,23 @@ import java.security.MessageDigest;
 import java.util.Arrays;
 
 public class F {
+    /**
+     * Describes the intended overwrite behavior when a file is created.
+     * <ul>
+     * <li>{@code NONE} throws an error</li>
+     * <li>{@code UNIQUE} causes a new unique filename to be created by appending a [n] suffix.</li>
+     * <li>{@code OVERWRITE} replaces the existing file.</li>
+     * </ul>
+     */
     public enum ClobberMode {NONE, UNIQUE, OVERWRITE;}
     
+    /**
+     * A "case" class describing a file clobbering result, including:
+     * <ul>
+     * <li>{@code file} the {@link File} that was created/written/matched</li>
+     * <li>{@code matched} is {@code true} if the existing {@link File} matched hashes and was retained</li>
+     * </ul>
+     */
     public static class Clobbered {
         public File    file;
         public boolean matched;
@@ -24,6 +39,20 @@ public class F {
             this.matched = matched;
         }
     }
+    /**
+     * Given an intended destination {@link File} and an existing {@code hash} (and the
+     * algorithm used to compute it), returns a {@link Clobbered} result indicating
+     * (a) whether the existing file matched the hash and should be used, and
+     * (b) the {@link File} to be used as the destination.  If an existing file was not
+     * matched, a new empty file will have been created (or an {@link IOException} thrown
+     * if the file can't be created or clobbered).
+     * @param dst the intended destination {@link File}
+     * @param mode the {@link ClobberMode} policy
+     * @param alg the hash algorithm
+     * @param hash the hash value (may be {@code null})
+     * @return a {@link Clobbered} result
+     * @throws IOException
+     */
     private static Clobbered clobber(File dst, ClobberMode mode, String alg, byte[] hash) throws IOException {
         boolean matched = false;
         if (dst.exists()) {
@@ -52,6 +81,18 @@ public class F {
         return new Clobbered(dst, matched);
     }
 
+    /**
+     * Conditionally copies {@link File} {@code src} to {@code dst} subject to the
+     * {@link ClobberMode} clobbering policy and an MD5 hash comparison if {@code dst}
+     * already exists.
+     * Note that {@code dst} may be a directory, in which case it is resolved by
+     * appending {@code src.getName()}.
+     * @param src the source file, which must exist and be a normal file
+     * @param dst the intended destination file (or directory), which may or may not exist already
+     * @param mode the {@link ClobberMode} policy
+     * @return a {@link Clobbered} indicating the match status and actual destination file
+     * @throws IOException
+     */
     public static Clobbered copy(File src, File dst, ClobberMode mode) throws IOException {
         if (!src.exists()) {
             throw new IOException("copy source does not exist");
@@ -78,6 +119,20 @@ public class F {
         return result;
     }
 
+    /**
+     * Conditionally downloads {@link URL} {@code src} to {@code dst} subject to the
+     * {@link ClobberMode} clobbering policy and a hash comparison if {@code dst}
+     * already exists.
+     * Note that {@code dst} may be a directory, in which case it is resolved by
+     * appending the final path element parsed from {@code src}.
+     * @param src the source {@link URL} as a {@link String}
+     * @param dst the intended destination file (or directory), which may or may not exist already
+     * @param alg the hash algorithm
+     * @param hash the hash for comparison (may be {@code null})
+     * @param mode the {@link ClobberMode} policy
+     * @return a {@link Clobbered} indicating the match status and actual destination file
+     * @throws Exception
+     */
     public static Clobbered download(String src, File dst, String alg, byte[] hash, ClobberMode mode) throws Exception {
         URL url = new URL(src);
         if (dst.isDirectory()) {
@@ -91,7 +146,10 @@ public class F {
             BufferedInputStream  in  = null;
             BufferedOutputStream out = null;
             try {
-                in  = new BufferedInputStream(url.openStream());
+                URLConnection u = new URL(src).openConnection();
+                u.setRequestProperty("User-Agent", "curl/7.37.1");
+                u.setUseCaches(false);
+                in = new BufferedInputStream(u.getInputStream());
                 out = new BufferedOutputStream(new FileOutputStream(result.file));
                 byte[] buf = new byte[65536];
                 int n;
@@ -106,6 +164,12 @@ public class F {
         return result;
     }
 
+    /**
+     * Downloads {@link URL} {@code src} and returns the content as a {@code byte[]},
+     * or {@code null} in case of an error (eating exceptions in the process).
+     * @param src the {@link URL} to download
+     * @return the content or {@code null}
+     */
     public static byte[] download(String src) {
         BufferedInputStream in = null;
         ByteArrayOutputStream out = null;
@@ -129,9 +193,23 @@ public class F {
         return null;
     }
 
+    /**
+     * Reads {@link File} named {@code f} and returns the contents as a {@code byte[]}.
+     * Throws an Exception in case of error.
+     * @param f the name of the {@link File} to read
+     * @return the file content
+     * @throws IOException
+     */
     public static byte[] read(String f) throws IOException {
         return read(new File(f));
     }
+    /**
+     * Reads {@link File} {@code f} and returns the contents as a {@code byte[]}.
+     * Throws an Exception in case of error.
+     * @param f the {@link File} to read
+     * @return the file content
+     * @throws IOException
+     */
     public static byte[] read(File f) throws IOException {
         byte buf[] = new byte[(int)f.length()];
         FileInputStream fis = null;
@@ -144,9 +222,25 @@ public class F {
         return buf;
     }
 
+    /**
+     * Writes {@link String} {@code content} to {@link File} named {@code f}, subject to
+     * the overwrite {@link ClobberMode} policy {@code mode}.
+     * @param content the content {@link String}
+     * @param f the name of destination {@link File}
+     * @param mode the overwrite policy
+     * @throws IOException if something goes wrong
+     */
     public static void write(String content, String f, ClobberMode mode) throws IOException {
         write(content, new File(f), mode);
     }
+    /**
+     * Writes {@link String} {@code content} to {@link File} {@code f}, subject to
+     * the overwrite {@link ClobberMode} policy {@code mode}.
+     * @param content the content {@link String}
+     * @param f the destination {@link File}
+     * @param mode the overwrite policy
+     * @throws IOException if something goes wrong
+     */
     public static void write(String content, File f, ClobberMode mode) throws IOException {
         f = clobber(f, mode, null, null).file;
         FileOutputStream fos = null;
@@ -158,12 +252,32 @@ public class F {
         }
     }
 
+    /**
+     * Returns the MD5 hash of the contents of {@link File} named {code f}.
+     * @param f the name of the {@code File} to read
+     * @return the MD5 hash
+     * @throws Exception in caes of error
+     */
     public static byte[] md5(String f) throws Exception {
         return md5(new File(f));
     }
+    /**
+     * Returns the MD5 hash of the contents of {@link File} {code f}.
+     * @param f the {@code File} to read
+     * @return the MD5 hash
+     * @throws Exception in case of error
+     */
     public static byte[] md5(File f) throws Exception {
         return hash(f, "MD5");
     }
+    /**
+     * Returns the hash of the contents of {@link File} {code f}
+     * using the indicated algorithm.
+     * @param f the {@code File} to read
+     * @param alg the hash algorithm to use
+     * @return the hash
+     * @throws Exception in case of error
+     */
     public static byte[] hash(File f, String alg) throws Exception {
         MessageDigest   md  = MessageDigest.getInstance(alg);
         FileInputStream fis = null;
@@ -179,6 +293,15 @@ public class F {
         }
         return md.digest();
     }
+    /**
+     * Return {@code true} if the hash of the contents of {@link File} {@code f}
+     * matches {@code hash} using algorithm {@code alg}.  Returns {@code} false
+     * otherwise, including if any Exceptions occur during the process.
+     * @param f the {@link File} to read
+     * @param alg the hash algorithm to use
+     * @param hash the expected hash value
+     * @return {@code true} if the hash matches
+     */
     public static boolean hashMatches(File f, String alg, byte[] hash) {
         if (f!=null && alg!=null && hash!=null && hash.length>0) {
             try {
@@ -188,6 +311,13 @@ public class F {
         }
         return false;
     }
+    /**
+     * Returns the hash of {@link File} {@code f} using algorithm {@code alg},
+     * or null if any error or Exception occurs.
+     * @param f the {@link File} to read
+     * @param alg the hash algorithm to use
+     * @return the hash value or {@code null}
+     */
     public static byte[] hashOrNull(File f, String alg) {
         try {
             return hash(f, alg);
@@ -195,6 +325,11 @@ public class F {
         return null;
     }
 
+    /**
+     * Formats a {@code byte[]} array as a hexadecimal {@link String}.
+     * @param bytes the {@code byte[]}s to format
+     * @return the formatted {@link String}, or {@code null} if {@code bytess} is {@code null}
+     */
     public static String hex(byte[] bytes) {
         if (bytes==null) return null;
         StringBuffer s = new StringBuffer();
@@ -219,6 +354,13 @@ public class F {
                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // D0-DF
                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // E0-EF
                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};// F0-FF
+    /**
+     * Converts a hexadecimal {@link String} into a binary {@code byte[]} array.  If the
+     * string is not of even length or is not 100% hexadecimal (or is {@code null},
+     * returns {@code null}.  Both upper and lower case A-F are accepted.
+     * @param s the {@link String} to convert
+     * @return the converted {@code byte[]}, or {@code null}
+     */
     public static byte[] hex(String s) {
         if (s==null || s.length() % 2 != 0 || s.contains("[^0-9a-fA-F]")) {
             return null;
